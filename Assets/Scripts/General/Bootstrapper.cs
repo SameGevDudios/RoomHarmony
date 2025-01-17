5f06a8bc279089e63c8d4fd0948ef9a89b661dd3
SameGevDudios <magomed.chapaev1@gmail.com>
2025-01-17 19:34:14 +0300

    Add input definition depending on a platform

--- Assets/Scripts/General/Bootstrapper.cs
+++ Assets/Scripts/General/Bootstrapper.cs
@@ -11,11 +11,24 @@ public class Bootstrapper : MonoBehaviour
 
     private void Awake()
     {
-        IInput input = new MobileInput();
+        IInput input = PlatformInput();
         IMover pickableMover = new Mover();
         IMoverValidator validator = new MoverValidator();
         ICameraMover cameraMover =
             new CameraMover(_cameraObject, _cameraDragSpeed, _cameraMinPosition, _cameraMaxPosition);
         _dragController.Constructor(input, pickableMover, validator, cameraMover);
+    }
+    private IInput PlatformInput()
+    {
+        switch (Application.platform)
+        {
+            case RuntimePlatform.WindowsEditor:
+                return new DesktopInput();
+            case RuntimePlatform.Android:
+                return new MobileInput();
+            default:
+                Debug.LogError("Unsupported platform. Cannot assign input type");
+                return null;
+        }
     }
 }
